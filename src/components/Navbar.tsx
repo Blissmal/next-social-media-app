88d8177d59bf6fdc1cdd16604c172b76aff70c01
Blissmal <malutibethuel@gmail.com>
2025-02-25 00:24:04 +0300

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,14 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return (
-        <div>Hello</div>
+        <div className="">
+            <div>
+                <Link href="/">BLISSOCIAL</Link>
+            </div>
+            <div className="hidden"></div>
+            <div></div>
+        </div>
     );
 }
 export default Navbar;
