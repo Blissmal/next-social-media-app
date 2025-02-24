09d27afb119646a8c8069a2af76ab35e65aeae04
Blissmal <malutibethuel@gmail.com>
2025-02-25 00:42:39 +0300

    Mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import MobileMenu from "@/components/MobileMenu";
+import {useState} from "react";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="h-24 flex items-center justify-between">
             <div>
-                <Link href="/" className="font-bold text-xl text-blue-500" >BLISSOCIAL</Link>
+                <Link href="/" className="font-bold text-xl text-blue-600" >BLISSOCIAL</Link>
             </div>
             <div className="hidden"></div>
             <div>
