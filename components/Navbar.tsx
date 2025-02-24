e72ac6fc5d39df0422fa54000cc063cf562eea46
Blissmal <malutibethuel@gmail.com>
2025-02-25 02:02:46 +0300

    updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import MobileMenu from "@/components/MobileMenu";
 import Image from "next/image";
+import {ClerkLoaded, ClerkLoading, SignedIn, SignedOut} from "@clerk/nextjs";
 
 const Navbar = () => {
     return (
@@ -27,7 +28,25 @@ const Navbar = () => {
             </div>
 
             <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
-                <MobileMenu />
+                <ClerkLoading>
+                    <div
+                        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                        role="status">
+  <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+  >Loading...</span
+  >
+                    </div>
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedIn>
+                        Signed in
+                    </SignedIn>
+                    <SignedOut>
+                        Signed out
+                    </SignedOut>
+                </ClerkLoaded>
+                <MobileMenu/>
             </div>
         </div>
     );
