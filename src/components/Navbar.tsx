3141769b04576029bda536d83b551371869494b7
Blissmal <malutibethuel@gmail.com>
2025-02-25 01:04:29 +0300

    update design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,26 +5,26 @@ import Image from "next/image";
 const Navbar = () => {
     return (
         <div className="h-24 flex items-center justify-between">
-            <div className="md:hidden lg:block">
+            <div className="md:hidden lg:block w-[20%]">
                 <Link href="/" className="font-bold text-xl text-blue-600" >BLISSOCIAL</Link>
             </div>
-            <div className="hidden md:flex">
+            <div className="hidden md:flex w-[50%]">
                 <div className="flex gap-6 text-gray-600">
-                    <Link href="/" className="flex gap-2">
-                        <Image src="/home.png" alt="Homepage" width={16} height={16} />
+                    <Link href="/" className="flex gap-2 items-center">
+                        <Image src="/home.png" alt="Homepage" width={16} height={16} className="size-4"/>
                         <span>Homepage</span>
                     </Link>
-                    <Link href="/" className="flex gap-2">
-                        <Image src="/friends.png" alt="Friends" width={16} height={16} />
+                    <Link href="/" className="flex gap-2 items-center">
+                        <Image src="/friends.png" alt="Friends" width={16} height={16} className="size-4"/>
                         <span>Friends</span>
                     </Link>
-                    <Link href="/" className="flex gap-2">
-                        <Image src="/stories.png" alt="Stories" width={16} height={16} />
+                    <Link href="/" className="flex gap-2 items-center">
+                        <Image src="/stories.png" alt="Stories" width={16} height={16} className="size-4"/>
                         <span>Stories</span>
                     </Link>
                 </div>
             </div>
-            <div>
+            <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
                 <MobileMenu />
             </div>
         </div>
