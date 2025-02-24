d4f13db05c5a7473ca884cd7db077b909e61c4cb
Blissmal <malutibethuel@gmail.com>
2025-02-25 00:59:56 +0300

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,29 @@
 import Link from "next/link";
 import MobileMenu from "@/components/MobileMenu";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <div className="h-24 flex items-center justify-between">
-            <div>
+            <div className="md:hidden lg:block">
                 <Link href="/" className="font-bold text-xl text-blue-600" >BLISSOCIAL</Link>
             </div>
-            <div className="hidden"></div>
+            <div className="hidden md:flex">
+                <div className="flex gap-6 text-gray-600">
+                    <Link href="/" className="flex gap-2">
+                        <Image src="/home.png" alt="Homepage" width={16} height={16} />
+                        <span>Homepage</span>
+                    </Link>
+                    <Link href="/" className="flex gap-2">
+                        <Image src="/friends.png" alt="Friends" width={16} height={16} />
+                        <span>Friends</span>
+                    </Link>
+                    <Link href="/" className="flex gap-2">
+                        <Image src="/stories.png" alt="Stories" width={16} height={16} />
+                        <span>Stories</span>
+                    </Link>
+                </div>
+            </div>
             <div>
                 <MobileMenu />
             </div>
