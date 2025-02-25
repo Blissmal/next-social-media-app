1ecac49568dc3cab5e97ae960cb793cd7d12c690
Blissmal <malutibethuel@gmail.com>
2025-02-25 12:34:23 +0300

    Styling and removing the scroll bars

--- components/Strories.tsx
+++ components/Strories.tsx
@@ -2,10 +2,58 @@ import Image from "next/image";
 
 const Strories = () => {
     return (
-        <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm">
-            <div className="flex gap-8">
+        <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hidden">
+            <div className="flex gap-8 w-max">
                 <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+                    <span className="font-medium">Ricky</span>
+                </div>
+                <div className="flex flex-col items-center gap-2 cursor-pointer">
+                    <Image src="/noAvatar.png" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
                     <span className="font-medium">Ricky</span>
                 </div>
             </div>
