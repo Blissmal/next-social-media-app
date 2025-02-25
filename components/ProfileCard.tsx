209ed0bae802e5e2ff1d973b1fc5100a5634f050
Blissmal <malutibethuel@gmail.com>
2025-02-25 17:49:03 +0300

    Complete home page

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -8,6 +8,18 @@ const ProfileCard = () => {
             <Image src="https://images.pexels.com/photos/29530548/pexels-photo-29530548/free-photo-of-scenic-florida-beach-with-palm-trees-and-lifeguard-hut.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='rounded-md object-cover'/>
             <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={48} height={48} className='rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10'/>
         </div>
+        <div className="h-20 flex flex-col gap-2 items-center">
+            <span className='font-semibold'>Edward Gabriel May</span>
+            <div className="flex items-center gap-4">
+                <div className='flex'>
+                    <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={12} height={12} className='rounded-full w-3 h-3'/>
+                    <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={12} height={12} className='rounded-full w-3 h-3'/>
+                    <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={12} height={12} className='rounded-full w-3 h-3'/>
+                </div>
+                <span className='text-xs text-gray-500'>500 Followers</span>
+            </div>
+            <button className='bg-blue-500 text-white text-xs p-2 rounded-md'>My Profile</button>
+        </div>
     </div>
   )
 }
