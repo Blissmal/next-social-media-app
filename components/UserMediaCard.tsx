edcafabe4459ef5858a6c207c4a8408d7fed6108
Blissmal <malutibethuel@gmail.com>
2025-02-25 17:09:37 +0300

    user media complete

--- components/UserMediaCard.tsx
+++ components/UserMediaCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,6 +8,31 @@ const UserMediaCard = ({userId}: {userId: string}) => {
       <div className="flex justify-between items-center font-medium">
         <span className='text-gray-500'>User Media</span>
         <Link href="/" className='text-blue-500 text-xs'>See all</Link>
+      </div>
+      <div className='flex gap-4 justify-between flex-wrap'>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div><div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
+        <div className="relative w-1/5 h-24">
+            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
+        </div>
       </div>
     </div>
   );
