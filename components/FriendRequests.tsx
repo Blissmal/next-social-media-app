a9faaf93a2f6b7e8eb10e4cfc6c1e34c83c9571a
Blissmal <malutibethuel@gmail.com>
2025-02-25 15:30:13 +0300

    class fix and friend request page section

--- components/FriendRequests.tsx
+++ components/FriendRequests.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const FriendRequests = () => {
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm'>
-      
+      <div className="flex justify-between items-center font-medium">
+        <span className='text-gray-500'>Friend Requests</span>
+        <Link href="/" className='text-blue-500 text-xs'>See all</Link>
+      </div>
+      <div className=""></div>
     </div>
   )
 }
