7bc69d140b86a973af563e05bd92e7ca6aec112c
Blissmal <malutibethuel@gmail.com>
2025-02-25 11:45:25 +0300

    Updated the main page containers

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className='flex gap-6'>
+      <div className="hidden xl:bloxk w-[20%]"></div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]"></div>
+      <div className="hidden lg:block w-[30%]"></div>
+    </div>
   )
 }
 
