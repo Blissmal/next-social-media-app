39f29f2406378f8882a9931def13e32a5fd9b645
Blissmal <malutibethuel@gmail.com>
2025-02-25 13:00:49 +0300

    shadow class

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AddPost = () => {
   return (
-    <div className='p-4 bg-white rounded-lg flex gap-4 justify-between text-sm'>
+    <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'>
       <Image src="" alt='' width={48} height={48} className="w-12 h-12 object-cover rounded-full ring-1" />
       <div className='flex-1'>
         <div className="flex gap-4">
