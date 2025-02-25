a94f49550214b302588377dcd8acd8784be9106f
Blissmal <malutibethuel@gmail.com>
2025-02-25 11:40:29 +0300

    updated navbar item

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="hidden md:flex w-[50%] text-sm">
+      <div className="hidden md:flex w-[50%] text-sm justify-between items-center">
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex gap-2 items-center">
             <Image
@@ -44,6 +44,10 @@ const Navbar = () => {
             />
             <span>Stories</span>
           </Link>
+        </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input type="text" placeholder="search..." className="bg-transparent outline-none"/>
+          <Image src="/search.png" alt="" width={14} height={14}/>
         </div>
       </div>
 
