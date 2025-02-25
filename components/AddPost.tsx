d0440eba62db9eadea582c267d465f80f36baaee
Blissmal <malutibethuel@gmail.com>
2025-02-25 14:50:10 +0300

    updated ui

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AddPost = () => {
   return (
     <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'>
-      <Image src="" alt='' width={48} height={48} className="w-12 h-12 object-cover rounded-full ring-1" />
+      <Image src="https://images.pexels.com/photos/20937745/pexels-photo-20937745/free-photo-of-couple-sitting-on-terrace-on-hilltop.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' width={48} height={48} className="w-12 h-12 object-cover rounded-full" />
       <div className='flex-1'>
         <div className="flex gap-4">
           <textarea placeholder='whats on your mind?' className='bg-slate-100 flex-1 p-2 rounded-lg'></textarea>
--- components/Comments.tsx
+++ components/Comments.tsx
@@ -11,7 +11,26 @@ const Comments = () => {
             <Image src="/emoji.png" alt='' width={16} height={16} className='w-4 h-4 cursor-pointer'/>
         </div>
       </div>
-      <div className=""></div>
+      <div className="">
+        <div className="flex gap-4 justify-between mt-6">
+            <Image src="https://images.pexels.com/photos/30465648/pexels-photo-30465648/free-photo-of-breathtaking-black-and-white-urban-window-cleaning.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' width={40} height={40} className='w-10 h-10 rounded-full'/>
+            <div className="flex flex-col gap-2 flex-1">
+                <span className='font-medium'>Beth mal</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, dignissimos. Ratione accusamus dolorem harum quibusdam quia maiores dolores voluptatibus impedit culpa reprehenderit molestias neque nisi, molestiae sit autem ea nesciunt.</p>
+                <div className='flex items-center gap-8 text-xs text-gray-500 mt-2'>
+                    <div className='flex items-center gap-4'>
+                        <Image src="/like.png"  alt="" width={16} height={16} className="cursor-pointer size-4" />
+                        <span className='text-gray-300'>|</span>
+                        <span className='text-gray-500'>123 Likes</span>
+                    </div>
+                    <div>
+                        Reply
+                    </div>
+                </div>
+            </div>
+            <Image src="/more.png"  alt="" width={16} height={16} className="cursor-pointer size-4" />
+        </div>
+      </div>
     </div>
   )
 }
