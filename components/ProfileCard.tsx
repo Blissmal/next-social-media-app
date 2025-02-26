be267475b0b5531843bbab25a785dd321b97610d
Blissmal <malutibethuel@gmail.com>
2025-02-26 16:52:01 +0300

    update

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -28,14 +28,14 @@ if (!user) return null;
             <Image src={user.avatar || "/noAvatar.png"} alt='' width={48} height={48} className='rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10'/>
         </div>
         <div className="h-20 flex flex-col gap-2 items-center">
-            <span className='font-semibold'>Edward Gabriel May</span>
+            <span className='font-semibold'>{(user.name && user.surname) ? user.name + "" + user.surname : user.username}</span>
             <div className="flex items-center gap-4">
                 <div className='flex'>
                     <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={12} height={12} className='rounded-full w-3 h-3'/>
                     <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={12} height={12} className='rounded-full w-3 h-3'/>
                     <Image src="https://images.pexels.com/photos/30739946/pexels-photo-30739946/free-photo-of-young-basketball-player-holding-ball-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' width={12} height={12} className='rounded-full w-3 h-3'/>
                 </div>
-                <span className='text-xs text-gray-500'>500 Followers</span>
+                <span className='text-xs text-gray-500'>{user._count.followers} Followers</span>
             </div>
             <button className='bg-blue-500 text-white text-xs p-2 rounded-md'>My Profile</button>
         </div>
