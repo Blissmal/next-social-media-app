987225be4a97a09062d583cf0c36a98323410c48
Blissmal <malutibethuel@gmail.com>
2025-02-27 00:13:04 +0300

    update to conditional profile update

--- components/UserInfoCard.tsx
+++ components/UserInfoCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import UserInfoCardInteraction from "./UserInfoCardInteraction";
+import UpdateUser from "./UpdateUser";
 
 const UserInfoCard = async ({ user }: { user: User }) => {
   const createdAtDate = new Date(user.createdAt);
@@ -50,9 +51,9 @@ const UserInfoCard = async ({ user }: { user: User }) => {
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       <div className="flex justify-between items-center font-medium">
         <span className="text-gray-500">User Information</span>
-        <Link href="/" className="text-blue-500 text-xs">
+        {currentUserId === user.id ? (<UpdateUser />) : (<Link href="/" className="text-blue-500 text-xs">
           See all
-        </Link>
+        </Link>)}
       </div>
       <div className="flex flex-col gap-4 text-gray-500">
         <div className="flex items-center gap-2">
