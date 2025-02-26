985ac08fa3179ecd0ae9001592b41fc191fba232
Blissmal <malutibethuel@gmail.com>
2025-02-26 18:59:02 +0300

    update

--- components/UserInfoCard.tsx
+++ components/UserInfoCard.tsx
@@ -1,8 +1,9 @@
+import { User } from '@prisma/client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const UserInfoCard = ({userId}: {userId: string}) => {
+const UserInfoCard = ({user}: {user: User}) => {
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
       <div className="flex justify-between items-center font-medium">
