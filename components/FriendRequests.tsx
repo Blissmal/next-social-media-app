d071308b37bada77112162beced37f428c82182f
Blissmal <malutibethuel@gmail.com>
2025-02-27 00:28:53 +0300

    update

--- components/FriendRequests.tsx
+++ components/FriendRequests.tsx
@@ -3,6 +3,7 @@ import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import FriendRequestList from './FriendRequestList'
 
 const FriendRequests = async () => {
 
@@ -26,16 +27,7 @@ const FriendRequests = async () => {
         <span className='text-gray-500'>Friend Requests</span>
         <Link href="/" className='text-blue-500 text-xs'>See all</Link>
       </div>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-            <Image src="https://images.pexels.com/photos/30112586/pexels-photo-30112586/free-photo-of-overlooking-a-ferris-wheel-at-a-bustling-fair.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' width={40} height={40} className='w-10 h-10 rounded-full object-cover'/>
-            <span className='font-semibold'>Wayne burt</span>
-        </div>
-        <div className="flex gap-3 justify-end">
-            <Image src="/accept.png" alt='' width={20} height={20} className='cursor-pointer'/>
-            <Image src="/reject.png" alt='' width={20} height={20} className='cursor-pointer'/>
-        </div>
-      </div>
+      <FriendRequestList requests={requests}/>
     </div>
   )
 }
