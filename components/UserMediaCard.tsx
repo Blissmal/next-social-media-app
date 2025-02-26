807b40ee4b0c254e7e23869abd8ed06d083f9e29
Blissmal <malutibethuel@gmail.com>
2025-02-26 21:43:18 +0300

    post map

--- components/UserMediaCard.tsx
+++ components/UserMediaCard.tsx
@@ -1,9 +1,26 @@
+import prisma from '@/lib/client';
 import { User } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
 
-const UserMediaCard = ({user}: {user: User}) => {
+const UserMediaCard = async ({user}: {user: User}) => {
+
+  console.log(user)
+
+  const postsWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null
+      }
+    },
+    take: 8,
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
       <div className="flex justify-between items-center font-medium">
@@ -11,29 +28,11 @@ const UserMediaCard = ({user}: {user: User}) => {
         <Link href="/" className='text-blue-500 text-xs'>See all</Link>
       </div>
       <div className='flex gap-4 justify-between flex-wrap'>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div><div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
-        <div className="relative w-1/5 h-24">
-            <Image src="https://images.pexels.com/photos/15910063/pexels-photo-15910063/free-photo-of-landscape-with-the-matterhorn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill className='object-cover rounded-md'/>
-        </div>
+        {postsWithMedia.length ? postsWithMedia.map(post => (
+          <div className="relative w-1/5 h-24" key={post.id}>
+            <Image src={post.img!} alt='' fill className='object-cover rounded-md'/>
+          </div>
+        )): "No media found!"}
       </div>
     </div>
   );
