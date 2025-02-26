8bcb798391a6e145d0a605dd4f6add5e60943fa9
Blissmal <malutibethuel@gmail.com>
2025-02-27 00:17:13 +0300

    update

--- components/FriendRequests.tsx
+++ components/FriendRequests.tsx
@@ -1,33 +1,30 @@
+import prisma from '@/lib/client'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const FriendRequests = () => {
+const FriendRequests = async () => {
+
+  const {userId} = await auth()
+  if (!userId) return null;
+
+  const requests = await prisma.followRequest.findMany({
+    where: {
+      receiverId: userId
+    },
+    include: {
+      sender: true
+    }
+  })
+
+  if (requests.length === 0) return null
+
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
       <div className="flex justify-between items-center font-medium">
         <span className='text-gray-500'>Friend Requests</span>
         <Link href="/" className='text-blue-500 text-xs'>See all</Link>
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-            <Image src="https://images.pexels.com/photos/30112586/pexels-photo-30112586/free-photo-of-overlooking-a-ferris-wheel-at-a-bustling-fair.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' width={40} height={40} className='w-10 h-10 rounded-full object-cover'/>
-            <span className='font-semibold'>Wayne burt</span>
-        </div>
-        <div className="flex gap-3 justify-end">
-            <Image src="/accept.png" alt='' width={20} height={20} className='cursor-pointer'/>
-            <Image src="/reject.png" alt='' width={20} height={20} className='cursor-pointer'/>
-        </div>
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-            <Image src="https://images.pexels.com/photos/30112586/pexels-photo-30112586/free-photo-of-overlooking-a-ferris-wheel-at-a-bustling-fair.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' width={40} height={40} className='w-10 h-10 rounded-full object-cover'/>
-            <span className='font-semibold'>Wayne burt</span>
-        </div>
-        <div className="flex gap-3 justify-end">
-            <Image src="/accept.png" alt='' width={20} height={20} className='cursor-pointer'/>
-            <Image src="/reject.png" alt='' width={20} height={20} className='cursor-pointer'/>
-        </div>
       </div>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
