e11de92c7176fbb6288aed16fa4222892def5c8b
Blissmal <malutibethuel@gmail.com>
2025-02-27 00:59:40 +0300

    Added accept and decline user actions

--- components/FriendRequestList.tsx
+++ components/FriendRequestList.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { acceptFollowRequest, declineFollowRequest } from "@/actions/user.acion";
 import { FollowRequest, User } from "@prisma/client";
 import Image from "next/image";
 import React, { useOptimistic, useState } from "react";
@@ -9,11 +10,28 @@ type RequestWithUser = FollowRequest & {
 
 const FriendRequestList = ({ requests }: { requests: RequestWithUser[] }) => {
     const [requestState, setRequestState] = useState(requests)
-
+    const accept = async (requestId: number, userId: string) => {
+        removeOPtimisticRequests(requestId)
+        try {
+            await acceptFollowRequest(userId)
+            setRequestState(prev => prev.filter(req => req.id !== requestId))
+        } catch (error) {
+            
+        }
+    }
+    const decline = async (requestId: number, userId: string) => {
+        removeOPtimisticRequests(requestId)
+        try {
+            await declineFollowRequest(userId)
+            setRequestState(prev => prev.filter(req => req.id !== requestId))
+        } catch (error) {
+            
+        }
+    }
     const [optimisticRequests, removeOPtimisticRequests] = useOptimistic(requestState, (state, value: number) => state.filter(req => req.id !== value))
   return (
     <div>
-      {requests.map((request) => (
+      {optimisticRequests.map((request) => (
         <div className="flex items-center justify-between" key={request.id}>
           <div className="flex items-center gap-4">
             <Image
@@ -26,20 +44,29 @@ const FriendRequestList = ({ requests }: { requests: RequestWithUser[] }) => {
             <span className="font-semibold">{request.sender.name && request.sender.surname ? request.sender.name + " " + request.sender.surname : request.sender.username}</span>
           </div>
           <div className="flex gap-3 justify-end">
-            <Image
-              src="/accept.png"
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
-            <Image
-              src="/reject.png"
-              alt=""
-              width={20}
-              height={20}
-              className="cursor-pointer"
-            />
+            <form action={() => accept(request.id, request.senderId)}>
+                <button>
+                <Image
+                    src="/accept.png"
+                    alt=""
+                    width={20}
+                    height={20}
+                    className="cursor-pointer"
+                />
+                </button>
+            </form>
+            
+            <form action={() => decline(request.id, request.senderId)}>
+                <button>
+                    <Image
+                      src="/reject.png"
+                      alt=""
+                      width={20}
+                      height={20}
+                      className="cursor-pointer"
+                    />
+                </button>
+            </form>
           </div>
         </div>
       ))}
