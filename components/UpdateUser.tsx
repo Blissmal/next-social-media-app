e2461a47246e501225b927aecacd8902c8658138
Blissmal <malutibethuel@gmail.com>
2025-03-01 23:50:04 +0300

    update

--- components/UpdateUser.tsx
+++ components/UpdateUser.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { User } from '@prisma/client'
-import React from 'react'
+import React, { useState } from 'react'
 
 const UpdateUser = ({user}: {user: User}) => {
+  const [open, setOpen] = useState(false)
   return (
     <div>
-      <span className='text-blue-500 text-xs cursor-pointer'>Update</span>
+      <span className='text-blue-500 text-xs cursor-pointer' onClick={() => setOpen(true)}>Update</span>
     </div>
   )
 }
