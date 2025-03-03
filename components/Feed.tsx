514bb0ba79273d67e4a01bb7ac1a9c996c6e686c
Blissmal <malutibethuel@gmail.com>
2025-03-03 19:21:32 +0300

    added feed fetch for profile and home page

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,50 @@
 import React from 'react'
 import Post from './Post'
+import { auth } from '@clerk/nextjs/server'
+import prisma from '@/lib/client'
 
-const Feed = ({username}: {username?: string}) => {
+const Feed = async ({username}: {username?: string}) => {
+  const {userId} = await auth()
+   let posts;
+
+   if (username) {
+    posts = await prisma.post.findMany({
+      where: {
+        user: {
+          username: username
+        }
+      },
+      include: {
+        user: true,
+        likes: {
+          select: {
+            userId: true
+          }
+        },
+        _count: {
+          select: {
+            comments: true
+          }
+        }
+      },
+      orderBy: {
+        createdAt: "desc"
+      }
+    })
+   }
+
+   if(!username && userId) {
+    const following = await prisma.follower.findMany({
+      where: {
+        followerId: userId
+      },
+      select: {
+        followerId: true
+      }
+    })
+    console.log(following)
+   }
+
   return (
     <div className='p-4 bg-white shadow-md rounded-lg flex flex-col gap-12'>
       <Post />
