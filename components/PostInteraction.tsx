57dc8671bca73de5ecd9aacfbc4482ff30a8bf29
Blissmal <malutibethuel@gmail.com>
2025-03-03 19:58:48 +0300

    updated code design

--- components/PostInteraction.tsx
+++ components/PostInteraction.tsx
@@ -1,37 +1,82 @@
-"use client"
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import { useAuth } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import React, { useOptimistic, useState } from "react";
 
-const PostInteraction = ({postId, likes, comments}: {postId: number, likes: string[], comments: number}) => {
+const PostInteraction = ({
+  postId,
+  likes,
+  comments,
+}: {
+  postId: number;
+  likes: string[];
+  comments: number;
+}) => {
+  const { isLoaded, userId } = useAuth();
+
+  const [likeState, setLikeState] = useState({
+    likeCount: likes.length,
+    isLiked: userId ? likes.includes(userId) : false,
+  });
+
+  const [optimisticLike, switchOptimisticLike] = useOptimistic(
+    likeState,
+    (state, value) => {
+      return {
+        likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+        isLiked: !state.isLiked,
+      };
+    }
+  );
   return (
     <div className="flex items-center justify-between text-sm my-4">
-        <div className="flex gap-8">
-            <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-                <Image src="/like.png" alt='' width={16} height={16} className='cursor-pointer'/>
-                <span className='text-gray-300'>|</span>
-                <span className='text-gray-500'>
-                    123 <span className='hidden md:inline'>Likes</span>
-                </span>
-            </div>
-            <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-                <Image src="/comment.png" alt='' width={16} height={16} className='cursor-pointer'/>
-                <span className='text-gray-300'>|</span>
-                <span className='text-gray-500'>
-                    123 <span className='hidden md:inline'>Comments</span>
-                </span>
-            </div>
-        </div>
-        <div className="">
+      <div className="flex gap-8">
         <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-                <Image src="/share.png" alt='' width={16} height={16} className='cursor-pointer'/>
-                <span className='text-gray-300'>|</span>
-                <span className='text-gray-500'>
-                    <span className='hidden md:inline'>Share</span>
-                </span>
-            </div>
+          <Image
+            src={optimisticLike.isLiked ? "/liked.png" : "/like.png"}
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500">
+            {optimisticLike.likeCount}{" "}
+            <span className="hidden md:inline">Likes</span>
+          </span>
+        </div>
+        <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+          <Image
+            src="/comment.png"
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500">
+            123 <span className="hidden md:inline">Comments</span>
+          </span>
         </div>
       </div>
-  )
-}
+      <div className="">
+        <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+          <Image
+            src="/share.png"
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500">
+            <span className="hidden md:inline">Share</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PostInteraction
+export default PostInteraction;
