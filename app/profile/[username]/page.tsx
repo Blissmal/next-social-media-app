5e16b237321f84d18150eb32978dbafe63d78803
Blissmal <malutibethuel@gmail.com>
2025-03-03 19:12:05 +0300

    Added username prop on feed

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -67,7 +67,7 @@ const ProfilePage = async ({params}: {params: {username: string}}) => {
               </div>
             </div>
           </div>
-          <Feed />
+          <Feed username={user.username}/>
         </div>
       </div>
 
