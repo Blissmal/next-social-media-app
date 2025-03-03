2ac0188f772d15f347d66cf572d1de13a22dbaed
Blissmal <malutibethuel@gmail.com>
2025-03-03 19:41:15 +0300

    post component handling

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,16 +10,16 @@ const Post = ({post}: {post: FeedPostType}) => {
     <div className='flex flex-col gap-4'>
       <div className="flex items-center justify-between">
         <div className='flex items-center gap-4'>
-            <Image src="https://images.pexels.com/photos/19324535/pexels-photo-19324535/free-photo-of-boat-sailing-along-a-treelined-city-canal.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' width={40} height={40} className='size-10 rounded-full'/>
-            <span className='font-medium'>Blissmal dev</span>
+            <Image src={post.user.avatar || "/noAvatar.png"} alt='' width={40} height={40} className='size-10 rounded-full'/>
+            <span className='font-medium'>{(post.user.name && post.user.surname) ? post.user.name + " " + post.user.surname : post.user.username}</span>
         </div>
         <Image src="/more.png" alt='' width={16} height={16} />
       </div>
       <div className="flex flex-col gap-4">
-        <div className="w-full min-h-96 relative">
-            <Image src="https://images.pexels.com/photos/30754133/pexels-photo-30754133/free-photo-of-stunning-aerial-view-of-london-skyline-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt='' fill className='object-cover rounded-md' />
-        </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quidem possimus neque perferendis non. Excepturi aperiam consequatur delectus, blanditiis repellat culpa deserunt illum saepe quaerat est voluptates earum modi dolorum!</p>
+        {post.img && <div className="w-full min-h-96 relative">
+            <Image src={post.img} alt='' fill className='object-cover rounded-md' />
+        </div>}
+        <p>{post.description}</p>
       </div>
       <div className="flex items-center justify-between text-sm my-4">
         <div className="flex gap-8">
@@ -43,7 +43,7 @@ const Post = ({post}: {post: FeedPostType}) => {
                 <Image src="/share.png" alt='' width={16} height={16} className='cursor-pointer'/>
                 <span className='text-gray-300'>|</span>
                 <span className='text-gray-500'>
-                    123 <span className='hidden md:inline'>Shares</span>
+                    <span className='hidden md:inline'>Share</span>
                 </span>
             </div>
         </div>
