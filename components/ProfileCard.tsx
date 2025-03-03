6d3e1a4ca96ca43b2882e5d917dda72dd4f853d1
Blissmal <malutibethuel@gmail.com>
2025-03-04 01:43:41 +0300

    updated

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/client";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
+import Link from "next/link";
 import React, { use } from "react";
 
 const ProfileCard = async () => {
@@ -72,9 +73,9 @@ const ProfileCard = async () => {
             {user._count.followers} Followers
           </span>
         </div>
-        <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+        <Link href={`/profile/${user.username}`} className="bg-blue-500 text-white text-xs p-2 rounded-md">
           My Profile
-        </button>
+        </Link>
       </div>
     </div>
   );
