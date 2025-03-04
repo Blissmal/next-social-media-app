d9c9e9eebf50125431c5515475a193ce2fd315b3
Blissmal <malutibethuel@gmail.com>
2025-03-05 00:19:49 +0300

    Post fetching

--- components/Strories.tsx
+++ components/Strories.tsx
@@ -1,6 +1,34 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const Strories = () => {
+const Strories = async () => {
+    const {userId: currentUserId} = await auth()
+    if (!currentUserId) return null
+    const stories = await prisma.story.findMany({
+        where: {
+            expiresAt: {
+                gt: new Date(),
+            },
+            OR: [
+                {
+                    user: {
+                        followers: {
+                            some: {
+                                followerId: currentUserId
+                            }
+                        }
+                    }
+                },
+                {
+                    userId: currentUserId
+                }
+            ]
+        },
+        include: {
+            user: true
+        }
+    })
     return (
         <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hidden">
             <div className="flex gap-8 w-max">
