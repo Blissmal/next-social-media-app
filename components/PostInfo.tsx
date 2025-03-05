35d531b7fb4d4273736ac6c3390d9d85d726364f
Blissmal <malutibethuel@gmail.com>
2025-03-05 20:03:19 +0300

    updated

--- components/PostInfo.tsx
+++ components/PostInfo.tsx
@@ -6,8 +6,25 @@ import React, { useState } from "react";
 const PostInfo = ({ postId }: { postId: number }) => {
   const [open, setOpen] = useState(false);
   return (
-    <div>
-      <Image src="/more.png" alt="" width={16} height={16} onClick={() => setOpen(prev => !prev)}/>
+    <div className="relative">
+      <Image
+        src="/more.png"
+        alt=""
+        width={16}
+        height={16}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div
+          className="absolute top-4 right-0 bg-white p-4 rounded-lg flex flex-col selection:gap-2 text-xs shadow-lg z-30"
+        >
+            <span className="cursor-pointer">View</span>
+            <span className="cursor-pointer">Repost</span>
+            <form action="">
+                <button>Delete</button>
+            </form>
+        </div>
+      )}
     </div>
   );
 };
