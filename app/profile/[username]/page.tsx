f381574d49aee5122181b401205b1fc15e3ef243
Blissmal <malutibethuel@gmail.com>
2025-03-10 01:34:58 +0300

    code refactor

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -9,14 +9,14 @@ import { notFound } from "next/navigation";
 interface Props {
   params: Promise<{
     username: string;
-  }>
-};
+  }>;
+}
 
-const ProfilePage = async ({params}: Props) => {
+const ProfilePage = async ({ params }: Props) => {
   const { username } = await params;
   const user = await prisma.user.findFirst({
     where: {
-      username
+      username,
     },
     include: {
       _count: {
@@ -24,40 +24,57 @@ const ProfilePage = async ({params}: Props) => {
           followers: true,
           following: true,
           posts: true,
-        }
-      }
-    }
-  })
-  if (!user) return notFound() 
+        },
+      },
+    },
+  });
+  if (!user) return notFound();
 
-    const {userId: currentUserId} = await auth()
+  const { userId: currentUserId } = await auth();
 
-    let isBlocked;
+  let isBlocked;
 
-    if (currentUserId) {
-      const res = await prisma.block.findFirst({
-        where: {
-          blockerId: user.id,
-          blockedId: currentUserId,
-        }
-      })
-      res ? isBlocked = true : isBlocked = false
-    }
+  if (currentUserId) {
+    const res = await prisma.block.findFirst({
+      where: {
+        blockerId: user.id,
+        blockedId: currentUserId,
+      },
+    });
+    res ? (isBlocked = true) : (isBlocked = false);
+  }
 
-    if (isBlocked) return notFound()
+  if (isBlocked) return notFound();
 
   return (
-      <div className='flex gap-6 pt-6'>
-      <div className="hidden xl:block w-[20%]"><LeftMenu type="profile"/></div>
+    <div className="flex gap-6 pt-6">
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu type="profile" />
+      </div>
 
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
           <div className="flex flex-col items-center justify-center">
             <div className="w-full h-64 relative">
-              <Image src={user.cover || "/noCover.png"} alt="" fill className="object-cover rounded-md"/>
-              <Image src={user.avatar || "/noAvatar.png"} alt="" width={128} height={128} className="size-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"/>
+              <Image
+                src={user.cover || "/noCover.png"}
+                alt=""
+                fill
+                className="object-cover rounded-md"
+              />
+              <Image
+                src={user.avatar || "/noAvatar.png"}
+                alt=""
+                width={128}
+                height={128}
+                className="size-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
             </div>
-            <h1 className="mt-20 mb-4 text-2xl font-medium">{(user.name && user.surname) ? user.name + " " + user.surname : user.username}</h1>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              {user.name && user.surname
+                ? user.name + " " + user.surname
+                : user.username}
+            </h1>
             <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
                 <span className="font-medium">{user._count.posts}</span>
@@ -73,12 +90,14 @@ const ProfilePage = async ({params}: Props) => {
               </div>
             </div>
           </div>
-          <Feed username={user.username}/>
+          <Feed username={user.username} />
         </div>
       </div>
 
-      <div className="hidden lg:block w-[30%]"><RightMenu user={user}/></div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu user={user} />
+      </div>
     </div>
-    );
-}
+  );
+};
 export default ProfilePage;
