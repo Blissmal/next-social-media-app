7ef77b5fab68dccb39bea71c5c8f0573c90f8c89
Blissmal <malutibethuel@gmail.com>
2025-03-10 01:26:23 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,18 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <ClerkProvider>
-            <html lang="en">
-                <body className={inter.className}>
-                    <div className="w-full bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-                        <Navbar />
-                    </div>
-                    <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-                        {children}
-                        <ToastContainer />
-                    </div>
-                </body>
-        </html>
-      </ClerkProvider>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="w-full bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            <Navbar />
+          </div>
+          <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            {children}
+            <ToastContainer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
